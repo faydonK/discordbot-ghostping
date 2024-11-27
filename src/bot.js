86e18661d1908ecd71f9424f3f7da48f45fadfbc
faydonK <milo.lequesne@gmail.com>
2024-11-27 11:02:33 +0100

    1.1.1

--- src/bot.js
+++ src/bot.js
@@ -34,7 +34,7 @@ export class Bot {
       console.log('💭 Use /help to see available commands');
 
         //bot's status | Comment or remove this line to disable the status or edit it
-        const statusMessage = 'V1.1.0';
+        const statusMessage = 'V1.1.1';
         this.client.user.setActivity(statusMessage, { type: ActivityType.Watching });
     });
 
